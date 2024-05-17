bcc3c7108e26b8c33b093f09846b16e9681a2796
Milind104 <202001246@daiict.ac.in>
2024-05-17 22:20:45 +0530

    Day-2 : Array,function basic, if-else , switch,All loops etc

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,23 +5,38 @@ namespace ConsoleApp1
 {
     public class Program
     {
+        // ------- FUNCTIONS ----------
+
+        static void PrintArray(int[] intArray, string mess)
+        {
+            foreach (int i in intArray)
+            {
+                Console.WriteLine("{0} : {1}",mess,i);
+            }
+        }
+        // ---- END OF FUNCTIONS --------
         static void Main(string[] args)
         {
-            //----------Console Methods----------
             /*
+         
+            //----------Console Methods----------
+            
              Console.ForegroundColor = ConsoleColor.Green;
              Console.BackgroundColor = ConsoleColor.White;
              Console.Clear();
              Console.WriteLine("Hello World");
-
              Console.Write("What is your name? ");
              string name = Console.ReadLine();
              Console.WriteLine($"Hello {name}");
+
             */
 
 
-            // ------ VARIABLES ------
+
             /*
+             
+            // ------ VARIABLES ------
+           
             // Variables are containers for storing data values.
 
             // Bools store true or false
@@ -80,8 +95,10 @@ namespace ConsoleApp1
             */
 
 
-            // ---------- DATA TYPE CONVERSION ----------
             /*
+             
+            // ---------- DATA TYPE CONVERSION ----------
+            
             // You can convert from string to other types with Parse
             bool boolFromStr = bool.Parse("True");
             int intFromStr = int.Parse("100");
@@ -107,18 +124,20 @@ namespace ConsoleApp1
             // smaller size type --> larger size
             int intNum = 10;
             long longNum = intNum;
+
             */
 
-            // ---------- FORMATTING OUTPUT ----------
+            /*// ---------- FORMATTING OUTPUT ----------
+
+
+            Console.WriteLine("Currency : {0:c}", 23.455);
+            Console.WriteLine("Pad with 0s : {0:d4}", 23);
+            Console.WriteLine("3 Decimals : {0:f3}", 23.4555);
+            Console.WriteLine("Commas : {0:n4}", 2300);*/
+
 
             /*
-             Console.WriteLine("Currency : {0:c}", 23.455);
-             Console.WriteLine("Pad with 0s : {0:d4}", 23);
-             Console.WriteLine("3 Decimals : {0:f3}", 23.4555);
-             Console.WriteLine("Commas : {0:n4}", 2300);
-             */
-
-            // ---------- STRINGS ----------
+             // ---------- STRINGS ----------
             // Strings store a series of characters
             string randString = "This is a string";
             //******String Formatting with Placeholders*******
@@ -129,24 +148,24 @@ namespace ConsoleApp1
 
             //Example Explained
             Console.WriteLine("String Contains is : {0}", randString.Contains("is"));
+            
+            // "String Contains is : {0}" is the format string.
+            // {0} is the placeholder that will be replaced by the first argument after the format string, which is randString.Contains("is") in this case.
+            // randString.Contains("is") evaluates to True or False depending on whether the substring "is" is found in randString.
+            // When randString is "This is a string", the expression randString.Contains("is") returns True, 
+  
+            //so the output will be:String Contains is : True
+            */
+
+
             /*
-             "String Contains is : {0}" is the format string.
-             {0} is the placeholder that will be replaced by the first argument after the format string, which is randString.Contains("is") in this case.
-             randString.Contains("is") evaluates to True or False depending on whether the substring "is" is found in randString.
-             When randString is "This is a string", the expression randString.Contains("is") returns True, 
-            
-            so the output will be:String Contains is : True
-             */
-
-
-          
-            
-
-            
+            // ---------- STRINGS ----------
+            // Strings store a series of characters
+            string randString = "This is a string";
             Console.WriteLine("String Length : {0}", randString.Length);
 
 
-            //The Contains method checks if a specified substring exists within the string.
+            The Contains method checks if a specified substring exists within the string.
             Console.WriteLine("String Contains is : {0}",
                 randString.Contains("is"));
 
@@ -203,7 +222,233 @@ namespace ConsoleApp1
             Console.Write(newString + "\n");
 
             Console.WriteLine(@"Exactly What I Typed\n");
+            */
 
+
+
+            // ------ ARRAYS ------
+            //Arrays are used to store multiple values in a single variable,
+            //instead of declaring separate variables for each value.
+
+            //create a array of size 3
+            int[] favNums = new int[3];
+
+            //add value to the arrary
+            favNums[0] = 26;
+            favNums[1] = 27;
+            favNums[2] = 28;
+            Console.WriteLine("favNum at index 0: {0} \n " +
+                "favNum at index 1:{1} \n " +
+                "favNum at index 2:{2}",
+                favNums[0], favNums[1], favNums[2]);
+
+            //craete and fill array
+            string[] customers = { "poorav", "milind", "krunal", "gaurang" };
+            Console.WriteLine(customers[1]);
+
+            //you can use var to create array but the values must be same type
+            var employees = new[] { "poorav", "milind", "krunal", "gaurang" };
+            Console.WriteLine(employees[0]);
+      
+
+            //create a array of base objects which is the 
+            //base type of all other types
+            object[] randomArray = { "Milind", 18, 3.14 };
+            Console.WriteLine(randomArray[0]);
+            Console.WriteLine(randomArray[1]);
+            Console.WriteLine(randomArray[2]);
+
+
+            // GetType knows its true type
+         
+            Console.WriteLine("random array 0:{0}  1:{1}", randomArray[0].GetType(), randomArray[1].GetType());
+
+            Console.WriteLine("random array 2:{0}", randomArray[2].GetType());
+
+            // Get number of items in array
+            Console.WriteLine("Array Size : {0}",randomArray.Length);
+
+
+            // ------for loop ----------
+            Console.WriteLine("\nFor Loop\n");
+
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+               
+                Console.WriteLine("Array {0} : Value : {1}" , i, randomArray[i]);
+            }
+
+            //------Multidimensional arrays----
+
+            string[,] custName = new string[2, 2] { { "Milind", "krunal" }, 
+                { "poorav", "gaurang" } };
+            Console.WriteLine("MD vlaue:{0}", custName[1,1]);
+            Console.WriteLine("Md vlaue:{0}", custName.GetValue(1, 1));
+            
+            //using for loop 
+
+            for(int i = 0;i < custName.GetLength(0);i++)
+            {
+                for(int j = 0;j < custName.GetLength(1);j++)
+                {
+                    Console.WriteLine("{0}",custName[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
+            // functions
+            int[] randNums = { 1, 4, 9, 2 };
+
+            PrintArray(randNums, "FoeeachLoop");
+
+            Console.WriteLine("------sort array-----------");
+            Array.Sort(randNums);
+            PrintArray(randNums, "sortArray");
+
+
+            Console.WriteLine("------reverse array-----------");
+            Array.Reverse(randNums);
+            PrintArray(randNums, "ReverseArray");
+
+            Console.WriteLine("------index of  array-----------");
+            Console.WriteLine("1 at index : {0} ",
+                Array.IndexOf(randNums, 1));
+
+            // Change value at index 1 to 0
+            randNums.SetValue(0, 1);
+            PrintArray(randNums, "change value at index");
+
+            // Copy part of an array to another
+            int[] srcArray = { 5, 2, 3 };
+            int[] destArray = new int[2];
+            int startInd = 0;
+            int length = 2;
+
+            Array.Copy(srcArray, startInd, destArray,
+                startInd, length);
+
+            PrintArray(destArray, "Copy");
+
+            // Create an array with CreateInstance
+            Array anotherArray = Array.CreateInstance(typeof(int), 11);
+
+            // Copy values in srcArray to destArray starting
+            // at index 5 in destination
+            srcArray.CopyTo(anotherArray, 5);
+
+            foreach (int m in anotherArray)
+            {
+                Console.WriteLine("CopyTo : {0} ", m);
+            }
+
+            // ----- If - else   -----
+            
+
+            int age = 10;
+
+            if ((age >= 5) && (age <= 7))
+            {
+                Console.WriteLine("Go to elementary school");
+            }
+
+            else if ((age > 7) && (age < 13))
+            {
+                Console.WriteLine("Go to middle school");
+            }
+
+            else if ((age > 13) && (age < 19))
+            {
+                Console.WriteLine("Go to high school");
+            }
+
+            else
+            {
+                Console.WriteLine("Go to college");
+            }
+
+            if ((age < 14) || (age > 67))
+            {
+                Console.WriteLine("You shouldn't work");
+            }
+
+            Console.WriteLine("! true = " + (!true));
+
+
+
+           //terneary operator
+            bool canDrive = age >= 16 ? true : false;
+
+
+
+
+
+            // -------- Switch -----------
+            
+            switch (age)
+            {
+                case 1:
+                case 2:
+                    Console.WriteLine("Go to Day Care");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Go to Preschool");
+                    break;
+                case 5:
+                    Console.WriteLine("Go to Kindergarten");
+                    break;
+                default:
+                    Console.WriteLine("Go to another school");
+                    goto OtherSchool;
+            }
+
+        OtherSchool:
+            Console.WriteLine("Elementary, Middle, High School");
+
+
+            // ----- WHILE LOOP -----
+           
+            int i = 1;
+            while (i <= 10)
+            {
+               
+                if (i % 2 == 0)
+                {
+                    i++;
+                    continue;
+                }
+
+                if (i == 9) break;
+
+                Console.WriteLine(i);
+                i++;
+            }
+
+            // ----- DO WHILE LOOP -----
+            // Use do while when you must execute the code at least once
+
+            // Generate a random number
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 11);
+            int numberGuessed = 0;
+            Console.WriteLine("Random Num : ", secretNumber);
+
+            do
+            {
+                Console.Write("Enter a number between 1 & 10 : ");
+
+                // Use Convert to switch the string into an int
+                // Other Convert options : ToBoolean, ToByte,
+                // ToChar, ToDecimal, ToDouble, ToInt64,
+                // ToString, and they can convert from any
+                // type to any other type
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+
+            } while (secretNumber != numberGuessed);
+
+            Console.WriteLine("You guessed it is was {0}",
+                secretNumber);
 
 
         }
