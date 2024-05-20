bc38ed134c0a8536905dd94e7c7fdd0a5c86d84d
Milind104 <202001246@daiict.ac.in>
2024-05-20 20:36:46 +0530

    DAY:5 -- Create a battle feature

---
+++ ConsoleApp7/Teleports.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal interface Teleports
+    {
+        string teleport();
+    }
+}
