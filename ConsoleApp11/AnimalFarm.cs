2b9b98d3e49067eab9d4b26c4417cc7ac516b010
Milind104 <202001246@daiict.ac.in>
2024-05-22 17:46:13 +0530

    DAY : 7 IEnumerable, Indexer, Opertor Overloading

---
+++ ConsoleApp11/AnimalFarm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+
+    //IEnumerable provides for iteration over a collection
+    class AnimalFarm : IEnumerable
+    {
+        private List<Animal> animalsList = new List<Animal>();
+        private AnimalFarm(List<Animal> animalList) 
+        {
+            this.animalsList = animalList;
+        }
+        public AnimalFarm() { }
+
+        //Indexer for Animalfarm created with this[]
+        public Animal this[int index]
+        {
+            get { return (Animal)animalsList[index]; }
+            set { animalsList.Insert(index, value); }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return animalsList.Count;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return animalsList.GetEnumerator();
+        }
+    }
+}
