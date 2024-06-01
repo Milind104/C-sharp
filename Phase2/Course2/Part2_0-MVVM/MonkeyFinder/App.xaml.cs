2471e950026207aa7c8d257058a6f4f6fe7ec19b
Milind104 <202001246@daiict.ac.in>
2024-06-02 01:15:27 +0530

    DAY:17 Simplifying MVVM with .NET Community Toolkit

---
+++ Phase2/Course2/Part2_0-MVVM/MonkeyFinder/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MonkeyFinder
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //var NavePage = new NavigationPage(new MainPage());
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Phase2/Course2/Part2_0-MVVM/MonkeyFinder/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using MonkeyFinder.Services;
+using MonkeyFinder;
+using MonkeyFinder.ViewModel;
+
+namespace MonkeyFinder
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MonkeyService>();
+            builder.Services.AddSingleton<MonkeysViewModel>();
+
+            return builder.Build();
+        }
+    }
+}
