cfbe121b77dd6eaf7c0abfa2f08b72509f8e90b6
Milind104 <202001246@daiict.ac.in>
2024-06-02 18:54:50 +0530

    DAY 18 Pull-to-Refresh,Empty-View

---
+++ Phase2/Course2/Part5-CollectionView/MonkeyFinder/MauiProgram.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using MonkeyFinder.Services;
+using MonkeyFinder;
+using MonkeyFinder.ViewModel;
+using MonkeyFinder.View;
+
+namespace MonkeyFinder
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+            builder.Services.AddSingleton<MonkeyService>();
+            builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+            builder.Services.AddSingleton<IMap>(Map.Default);
+
+            builder.Services.AddTransient<MainPage>();
+            builder.Services.AddTransient<MonkeysViewModel>();
+            builder.Services.AddTransient<MonkeyDetailsViewModel>();
+            builder.Services.AddTransient<DetailsPage>();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Phase2/Course2/Part5-CollectionView/MonkeyFinder/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MonkeyFinder
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
