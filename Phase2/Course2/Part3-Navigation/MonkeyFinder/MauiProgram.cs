23bdafe358cdf566138cca6286becf89078dfde5
Milind104 <202001246@daiict.ac.in>
2024-06-02 11:28:40 +0530

    DAY 18 Navigation With .NET MAUI Shell

---
+++ Phase2/Course2/Part3-Navigation/MonkeyFinder/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using MonkeyFinder.Services;
+using MonkeyFinder;
+using MonkeyFinder.ViewModel;
+using MonkeyFinder.View;
+
+namespace MonkeyFinder
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+            builder.Services.AddTransient<MainPage>();
+            builder.Services.AddSingleton<MonkeyService>();
+            builder.Services.AddTransient<MonkeysViewModel>();
+            builder.Services.AddTransient<MonkeyDetailsViewModel>();
+            builder.Services.AddTransient<DetailsPage>();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Phase2/Course2/Part3-Navigation/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MonkeyFinder.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyFinder.ViewModel
+{
+    [QueryProperty(nameof(Monkey),"Monkey")]
+    public partial class MonkeyDetailsViewModel:BaseViewModel
+    {
+        public MonkeyDetailsViewModel()
+        {
+            
+        }
+        [ObservableProperty]
+        Monkey monkey;
+    }
+}
