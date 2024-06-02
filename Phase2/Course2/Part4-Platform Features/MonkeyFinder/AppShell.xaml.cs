0291ec2981dec863e9e67875f6b41a8ce894cbf9
Milind104 <202001246@daiict.ac.in>
2024-06-02 16:10:18 +0530

    DAY:18 Platform Features and Device Sensors

---
+++ Phase2/Course2/Part4-Platform Features/MonkeyFinder/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MonkeyFinder.View;
+
+namespace MonkeyFinder
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(DetailsPage), typeof(DetailsPage));
+        }
+    }
+}
